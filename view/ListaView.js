13b98fcc6417eb481b4be9ff2c7b6c1df5952cf0
christuss <szendefi.krisztofer@gmail.com>
2023-11-05 16:48:29 +0100

    fejlec listaba

--- view/ListaView.js
+++ view/ListaView.js
@@ -5,12 +5,25 @@ class ListaView {
         this.szuloElem = szuloElem;
         this.lista = lista;
         this.szuloElem.append("<table class='table table-bordered'></table>");
+        this.tabla = this.szuloElem.children("table");
+
     }
 
     megjelenites() {
+        this.fejlec(this.lista[0]);
         this.lista.forEach((elem, index) => {
-            new ListaSorView(elem, $(".table"), index);
+            new ListaSorView(elem, this.tabla, index);
         });
+    }
+
+    fejlec(elem) {
+        let s = "<tr>";
+        for (let index = 0; index < Object.keys(elem).length; index++) {
+            s += "<th>" + Object.keys(elem)[index] + "</th>";
+           }
+    
+          s+= '<th>kesz</th><th>kuka</th></tr>'
+          this.tabla.append(s);
     }
 }
 export default ListaView;
